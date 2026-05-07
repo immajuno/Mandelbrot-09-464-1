182ef89b54b40ad22df127cc3d26f9e69f8208b8
immajuno <bayazitov.timr@yandex.ru>
2026-05-07 07:33:32 +0300

    Update PaintPanel.java

--- src/ru/gr0946x/ui/PaintPanel.java
+++ src/ru/gr0946x/ui/PaintPanel.java
@@ -1,10 +1,6 @@
 package ru.gr0946x.ui;
 
 import ru.gr0946x.ui.painting.Painter;
-<<<<<<< HEAD
-
-=======
->>>>>>> 3dbaa6c (Внесены итоговые правки, исправлены ошибки в коде, исправлены неработающие функции лабы)
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
@@ -12,12 +8,8 @@ import java.awt.event.ComponentEvent;
 
 public class PaintPanel extends JPanel {
 
-<<<<<<< HEAD
-    private Painter painter;
-=======
     protected Painter painter;
 
->>>>>>> 3dbaa6c (Внесены итоговые правки, исправлены ошибки в коде, исправлены неработающие функции лабы)
     public PaintPanel(Painter painter){
         this.painter = painter;
         addComponentListener(new ComponentAdapter() {
@@ -36,9 +28,5 @@ public class PaintPanel extends JPanel {
         super.paint(g);
         painter.paint(g);
     }
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 3dbaa6c (Внесены итоговые правки, исправлены ошибки в коде, исправлены неработающие функции лабы)
 }
+
