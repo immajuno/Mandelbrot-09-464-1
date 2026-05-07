cbc58553d3c149809d72cd27c9c44eacf0ccd92b
immajuno <bayazitov.timr@yandex.ru>
2026-05-07 09:31:55 +0300

    Update Converter.java

--- src/ru/gr0946x/Converter.java
+++ src/ru/gr0946x/Converter.java
@@ -11,6 +11,7 @@ public class Converter {
         this.yMax = yMax;
         this.width = 800;
         this.height = 600;
+        keepProportions();
     }
 
     public double xScr2Crt(int x) { return xMin + x * (xMax - xMin) / width; }
@@ -23,8 +24,36 @@ public class Converter {
     public int getWidth() { return width; }
     public int getHeight() { return height; }
 
-    public void setXShape(double xMin, double xMax) { this.xMin = xMin; this.xMax = xMax; }
-    public void setYShape(double yMin, double yMax) { this.yMin = yMin; this.yMax = yMax; }
-    public void setWidth(int width) { this.width = width; }
-    public void setHeight(int height) { this.height = height; }
+    public void setXShape(double xMin, double xMax) {
+        this.xMin = xMin;
+        this.xMax = xMax;
+        keepProportions();
+    }
+
+    public void setYShape(double yMin, double yMax) {
+        this.yMin = yMin;
+        this.yMax = yMax;
+        keepProportions();
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+        keepProportions();
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+        keepProportions();
+    }
+
+    private void keepProportions() {
+        if (width <= 0 || height <= 0) return;
+
+        double widthRe = xMax - xMin;
+        double requiredHeight = widthRe * height / width;
+        double centerIm = (yMin + yMax) / 2;
+
+        yMin = centerIm - requiredHeight / 2;
+        yMax = centerIm + requiredHeight / 2;
+    }
 }
