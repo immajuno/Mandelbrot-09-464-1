74decc7fb219a6d5f0fdbd2dec475fd9eb188720
immajuno <bayazitov.timr@yandex.ru>
2026-05-07 07:33:51 +0300

    Update Mandelbrot.java

--- src/ru/gr0946x/ui/fractals/Mandelbrot.java
+++ src/ru/gr0946x/ui/fractals/Mandelbrot.java
@@ -2,19 +2,6 @@ package ru.gr0946x.ui.fractals;
 
 import ru.smak.math.Complex;
 
-<<<<<<< HEAD
-import static java.lang.Math.max;
-import static java.lang.Math.sqrt;
-
-public class Mandelbrot implements Fractal{
-
-    private final int maxIterations = 100;
-    private final double R2 = 4;
-    public double getR(){
-        return sqrt(R2);
-    }
-
-=======
 import static java.lang.Math.sqrt;
 
 public class Mandelbrot implements Fractal {
@@ -34,21 +21,11 @@ public class Mandelbrot implements Fractal {
         this.maxIterations = maxIterations;
     }
 
->>>>>>> 3dbaa6c (Внесены итоговые правки, исправлены ошибки в коде, исправлены неработающие функции лабы)
     @Override
     public float inSetProbability(double x, double y) {
         var c = new Complex(x, y);
         var z = new Complex();
         int i = 0;
-<<<<<<< HEAD
-        while (z.getAbsoluteValue2() < R2 && ++i < maxIterations){
-            z.timesAssign(z);
-            z.plusAssign(c);
-        }
-        return (float)i / maxIterations;
-    }
-}
-=======
         while (z.getAbsoluteValue2() < R2 && ++i < maxIterations) {
             z.timesAssign(z);
             z.plusAssign(c);
@@ -56,4 +33,3 @@ public class Mandelbrot implements Fractal {
         return (float) i / maxIterations;
     }
 }
->>>>>>> 3dbaa6c (Внесены итоговые правки, исправлены ошибки в коде, исправлены неработающие функции лабы)
